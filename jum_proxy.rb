45106faed265c3c76bb32a0aff73b12087704760
leonardofl87 <leonardofl87@4d5c199f-a5e8-6b79-82b6-10b046ba1266>
2010-08-13 00:09:05 +0000

    Licença adicionada: GPL v3 ;)

--- jum_proxy.rb
+++ jum_proxy.rb
@@ -1,6 +1,23 @@
+#############################################################################
+#
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################
+
 # Mechanize tests with Orkut/Twitter
 # @author Davis Zanetti Cabral
-# @author Leonardo Alexandre Ferreira Leite
+# @author Leonardo Alexandre Ferreira Leite (leonardofl87@gmail.com)
 # Davis original: http://gist.github.com/104221
 
 # Orkut usage example:
